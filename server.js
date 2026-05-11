d51b034558708c56470a110f3b3885b0f0708247
hammadsatti080 <hammadsatti080@gmail.com>
2026-05-10 23:02:37 -0700

    Updated backend config fle

--- server.js
+++ server.js
@@ -48,16 +48,8 @@ connectDB();
 const app = express();
 
 // Middleware
-//app.use(cors());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://ecommerence-one.vercel.app"
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
